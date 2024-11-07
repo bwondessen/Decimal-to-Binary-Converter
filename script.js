868967876415bb9815679e198faf702e5404ba31
bwondessen <bwondessen23@gmail.com>
2024-11-06 19:38:51 -0600

    Alert user when input is invalid

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
+    if (
+        !numberInput.value ||
+        isNaN(parseInt(numberInput.value)) ||
+        parseInt(numberInput.value) < 0
+    ) {
+        alert("Please provide a decimal number greater than or equal to 0");
+    }
+
     console.log(numberInput.value);
 };
 
