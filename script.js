36e37cccc58507e43dc921e3745869993c2c00bd
bwondessen <bwondessen23@gmail.com>
2024-11-07 19:16:25 -0600

    End checkUserInput function after alerting user

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+
+
 const checkUserInput = () => {
     if (
         !numberInput.value ||
@@ -9,6 +11,7 @@ const checkUserInput = () => {
         parseInt(numberInput.value) < 0
     ) {
         alert("Please provide a decimal number greater than or equal to 0");
+        return;
     }
 
     console.log(numberInput.value);
