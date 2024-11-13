9ced9e402c1bd8f2c6f2e9264c27898b3c69ad6b
bwondessen <bwondessen23@gmail.com>
2024-11-12 19:14:20 -0600

    Make checkUserInput function more efficient

--- script.js
+++ script.js
@@ -10,17 +10,28 @@ const decimalToBinary = (input) => {
     }
 };
 
+const showAnimation = () => {
+
+};
+
 const checkUserInput = () => {
+    const inputInt = parseInt(numberInput.value);
+
     if (
         !numberInput.value ||
-        isNaN(parseInt(numberInput.value)) ||
-        parseInt(numberInput.value) < 0
+        isNaN(inputInt) ||
+        inputInt < 0
     ) {
         alert("Please provide a decimal number greater than or equal to 0");
         return;
     }
 
-    result.textContent = decimalToBinary(parseInt(numberInput.value));
+    if (inputInt === 5) {
+        showAnimation();
+        return;
+    }
+
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = "";
 };
 
